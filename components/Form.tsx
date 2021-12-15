69e3afcdf387ac911d036ac73c153ea213b0a086
DarioMarret <64284053+DarioMarret@users.noreply.github.com>
2021-12-15 11:07:00 -0500

    tercer commit

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,51 +1,89 @@
 import { useState, useEffect, ChangeEvent } from "react"
 import { Col, Container, Row, Form, Button, InputGroup, FormControl, Modal as ModalSpinner } from "react-bootstrap";
-import { FiUser, FiKey } from "react-icons/fi";
+import {  FiAirplay, FiUser, FiUsers, FiAtSign, FiPhone, FiArrowRight, FiBriefcase } from "react-icons/fi";
 import axios from "axios"
 
+interface TipoIden {
+    id:number,
+    codigo:string,
+    tipo_identificacion:string,
+}
+
 export default function FormCliente() {
+
+    const [tipoIden, settipoIden] = useState<TipoIden[]>([])
+    const [empresa, setempresa] = useState(null)
     const [Cliente, setCliente] = useState({
         cedula:null,
         nombres:null,
         apellidos:null,
-        email:null,
-        telefono:null,
+        correo:null,
+        contacto_1:null,
         direccion:null,
         razon_social:null,
         tipo_identificacion_proveedor:null,
+        empresa:null,
     })
+    useEffect(() => {
+        var pathname = window.location.search;
+        let n_path = pathname.split("?empresa=")[1].split("%20").join(',').replaceAll(',', ' ')
+        setCliente({
+            ...Cliente,
+            ['empresa']:n_path
+        })
+        setempresa(n_path)
+        Init()
+    }, [])
 
+    const Init=async() => {
+        const { data } = await axios.get(`https://sistemabmdigital.com:4000/listar_tipo_identificacion`)
+        var n_tipo = data.data.filter(iten => iten.codigo != "07")
+        
+       settipoIden(n_tipo)
+    }
     const handleTextImput = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.target.name === 'cedula' && e.target.value.length === 10){
-            axios.get(`https://turnos.manta.gob.ec/consultacedula/`+e.target.value,
-            {
-                headers: {
-                    'Accept':'*/*',
-                }
-            })
-            .then(response =>{
-                console.log(response)
-            }).catch(err=>{
-                console.log(err)
-            })
-        }
+        setCliente({
+            ...Cliente,
+            [e.target.name]:e.target.value
+        })
+    }
+    const handleTextImputSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+        setCliente({
+            ...Cliente,
+            [e.target.name]:e.target.value
+        })
     }
     return (
         <Container>
             {/* <SpinnerCargando visible={cargando}/> */}
-            <h1 className="d-flex justify-content-end shadow-lg text-primary mt-5 p-3 text-center rounded">
+            {/* <h1 className="d-flex justify-content-end shadow-lg text-primary mt-5 p-3 text-center rounded">
                 <b>Registrar</b>
                 <b>Menu</b>
-            </h1>
+            </h1> */}
             <Row className="mt-5 justify-content-center align-items-center">
                 <Col lg={5} md={6} sm={12}
                     className="p-5 m-auto shadow-lg rounded-lg "
                 >
+                    <h3 className="text-center">{empresa}</h3>
                     <i>Registrar datos para la emision de su factura electronico cuidando el ecosistema </i>
                     <Form>
+                    <Form.Label>Tipo de Indentificacion:</Form.Label>
+                        <InputGroup className="mb-1">
+                            <InputGroup.Text id="basic-addon1"><FiAirplay /></InputGroup.Text>
+                            <select name="tipo_identificacion_proveedor" id="" className="form-control" onChange={handleTextImputSelect}>
+                                <option value={null}>Seleccione un tipo de identificacion</option>
+                                {
+                                    tipoIden.map((iten, index) =>(
+                                        <option value={iten.codigo} key={index}>{iten.tipo_identificacion}</option>
+                                    ))
+                                }
+                            </select>
+                        </InputGroup>
+
+
                         <Form.Label>Cedula/Ruc:</Form.Label>
                         <InputGroup className="mb-1">
-                            <InputGroup.Text id="basic-addon1"><FiUser /></InputGroup.Text>
+                            <InputGroup.Text id="basic-addon1"><FiAirplay /></InputGroup.Text>
                             <FormControl
                                 placeholder="Ingresar cedula o ruc"
                                 name="cedula"
@@ -55,7 +93,7 @@ export default function FormCliente() {
 
                         <Form.Label>Nombres:</Form.Label>
                         <InputGroup className="mb-1">
-                            <InputGroup.Text id="basic-addon2"><FiKey /></InputGroup.Text>
+                            <InputGroup.Text id="basic-addon2"><FiUser /></InputGroup.Text>
                             <FormControl
                                 placeholder="Ingresar nombres completo"
                                 type="text"
@@ -66,7 +104,7 @@ export default function FormCliente() {
 
                         <Form.Label>Apellido:</Form.Label>
                         <InputGroup className="mb-1">
-                            <InputGroup.Text id="basic-addon2"><FiKey /></InputGroup.Text>
+                            <InputGroup.Text id="basic-addon2"><FiUsers /></InputGroup.Text>
                             <FormControl
                                 placeholder="ingresar apellidos completo"
                                 type="text"
@@ -77,7 +115,7 @@ export default function FormCliente() {
 
                         <Form.Label>Correo:</Form.Label>
                         <InputGroup className="mb-1">
-                            <InputGroup.Text id="basic-addon2"><FiKey /></InputGroup.Text>
+                            <InputGroup.Text id="basic-addon2"><FiAtSign /></InputGroup.Text>
                             <FormControl
                                 placeholder="ingresar coreo electronico"
                                 type="email"
@@ -88,7 +126,7 @@ export default function FormCliente() {
 
                         <Form.Label>Contacto:</Form.Label>
                         <InputGroup className="mb-1">
-                            <InputGroup.Text id="basic-addon2"><FiKey /></InputGroup.Text>
+                            <InputGroup.Text id="basic-addon2"><FiPhone /></InputGroup.Text>
                             <FormControl
                                 placeholder="Numero de contacto"
                                 type="text"
@@ -99,7 +137,7 @@ export default function FormCliente() {
 
                         <Form.Label>Razon Social:</Form.Label>
                         <InputGroup className="mb-1">
-                            <InputGroup.Text id="basic-addon2"><FiKey /></InputGroup.Text>
+                            <InputGroup.Text id="basic-addon2"><FiBriefcase /></InputGroup.Text>
                             <FormControl
                                 placeholder="Razon social o nombre comercial"
                                 type="text"
@@ -110,7 +148,7 @@ export default function FormCliente() {
 
                         <Form.Label>Direccion:</Form.Label>
                         <InputGroup className="mb-1">
-                            <InputGroup.Text id="basic-addon2"><FiKey /></InputGroup.Text>
+                            <InputGroup.Text id="basic-addon2"><FiArrowRight /></InputGroup.Text>
                             <FormControl
                                 placeholder="ingresar una direccion"
                                 type="text"
@@ -123,17 +161,21 @@ export default function FormCliente() {
                             variant="primary btn-block"
                             type="button"
                             className="form-control"
-                        //   onClick={(e)=>btn_login_on_click(e)}
+                        //onClick={(e)=>btn_login_on_click(e)}
                         >
                             Registrar
                         </Button>
                     </Form>
                 </Col>
             </Row>
-            {/* ) : (
-        //   <ConfirmacionValidacion /> */}
-            {/* )} */}
-
         </Container>
     )
+}
+export async function getStaticProps({params}) {
+    try {
+        console.log(params);
+        
+    } catch (error) {
+        console.log(error);
+    }
 }
